c8f86353050fb761154b3f7e9ce68215e59c5a1c
Nenad Trajkov <nenad.trajkov@gmail.com>
2021-05-06 16:52:07 +0200

    decresed the size of sprites

--- WorldEnviorments/water/WaterReflection.gd
+++ WorldEnviorments/water/WaterReflection.gd
@@ -12,7 +12,7 @@ func _process(delta):
 	zoom_changed()
 
 func zoom_changed():
-	material.set_shader_param("y_zoom", get_viewport().global_canvas_transform.y.y/4)
+	material.set_shader_param("y_zoom", get_viewport().global_canvas_transform.y.y)
 	
 
 func _on_WaterReflection_item_rect_changed():
